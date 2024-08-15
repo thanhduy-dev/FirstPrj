c16cd109c97d75e80c90a4af5171f28e8d7ca4a0
Thanh Duy <23673181.duy@student.iuh.edu.vn>
2024-08-16 00:42:08 +0700

    Do Thanh Duy

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Do Thanh Duy!");
+        System.out.println("Welcome to the DHKTMP19B!");
+    }
+}
